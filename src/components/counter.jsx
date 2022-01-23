8b967520f1fa6b480f6d975f3ecaa2f32c8b864c
Tareek <trksolomon21@gmail.com>
2022-01-23 14:49:40 -0500

    init

---
+++ src/components/counter.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+class Counter extends React.Component {
+  state = {
+    count: 0,
+  };
+
+  // constructor(){
+  //   super();
+  //   this.handleIncrement = this.handleIncrement.bind(this);
+  // }
+
+  handleIncrement = (product) => {
+    console.log(product);
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement(product)}
+          className="btn btn-secondary btn-sm">
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) {
+      return <p>There are no tags!</p>;
+    }
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default Counter;
